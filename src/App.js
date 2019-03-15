bbeae06b0267f5b678ddbd53f67a275656a50f6a
John Barhorst <crashdsinc@comcast.net>
2019-03-14 22:26:48 -0500

    refactored API calls, made profile search function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-
 import React, { Component } from 'react';
-
 
 export default class App extends Component {
   constructor(props) {
@@ -9,136 +7,77 @@ export default class App extends Component {
       charData: {},
       historicalStats: {},
       profile: {},
-      manifest: {},
-      url: 'https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/1/CrashXVII/',
+      charSearch: '',
+      url:
+        'https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/1/CrashXVII/',
+      baseUrl: 'https://www.bungie.net/Platform/Destiny2/',
     };
   }
 
-  getProfile = async () => {
+  apiCall = async (urlFinish) => {
     try {
-      const { profile, url } = this.state;
-      const response = await fetch(url, {
+      const { baseUrl } = this.state;
+      const response = await fetch(`${baseUrl}${urlFinish}`, {
         method: 'GET',
         headers: {
-          'X-API-Key':
-            '5d7089e50cbe489d8e4da672a35a3bd1',
+          'X-API-Key': '5d7089e50cbe489d8e4da672a35a3bd1',
         },
       });
       const json = await response.json();
       const results = await json.Response;
-      console.log(results);
-      this.setState(
-        { profile: results[0] },
-        console.log(profile),
-      );
+      return results;
     } catch (e) {
       throw new Error(e);
     }
   };
 
-  getManifest = async () => {
-    try {
-      const { manifest } = this.state;
-      const response = await fetch(
-        'https://www.bungie.net/Platform/Destiny2/Manifest/',
-        {
-          method: 'GET',
-          headers: {
-            'X-API-Key':
-              '5d7089e50cbe489d8e4da672a35a3bd1',
-          },
-        },
-      );
-      const json = await response.json();
-      const results = await json.Response;
-      console.log(results);
-      this.setState(
-        { manifest: results },
-        console.log(manifest),
-      );
-    } catch (e) {
-      throw new Error(e);
-    }
+  getProfile = async () => {
+    const { charSearch } = this.state;
+    const profile = await this.apiCall(`SearchDestinyPlayer/1/${charSearch}/`);
+    this.setState({
+      profile,
+    });
   };
 
   getHistoricalStats = async () => {
-    try {
-      const response = await fetch(
-        'https://www.bungie.net/Platform/Destiny2/1/Account/4611686018434143187/Stats/',
-        {
-          method: 'GET',
-          headers: {
-            'X-API-Key': '5d7089e50cbe489d8e4da672a35a3bd1',
-          },
-        },
-      );
-      const json = await response.json();
-      const results = await json.Response;
-      console.log(results);
-      this.setState(
-        { historicalStats: results },
-      );
-    } catch (e) {
-      throw new Error(e);
-    }
+    const historicalStats = await this.apiCall(
+      '1/Account/4611686018434143187/Stats/',
+    );
+    this.setState({
+      historicalStats,
+    });
   };
 
   getCharacterData = async () => {
-    try {
-      const { charData } = this.state;
-      const response = await fetch(
-        'https://www.bungie.net/Platform/Destiny2/1/Profile/4611686018434143187/Character/2305843009271724646/?components=Characters,CharacterActivities',
-        {
-          method: 'GET',
-          headers: {
-            'X-API-Key': '5d7089e50cbe489d8e4da672a35a3bd1',
-          },
-        },
-      );
-      const json = await response.json();
-      const results = await json.Response;
-      console.log(results);
-      this.setState({ charData: results },
-        console.log(charData));
-    } catch (e) {
-      throw new Error(e);
-    }
+    const charData = await this.apiCall(
+      '1/Profile/4611686018434143187/Character/2305843009271724646/?components=Characters,CharacterActivities',
+    );
+    this.setState({
+      charData,
+    });
+  };
+
+  handleInput = (e) => {
+    this.setState({ charSearch: e.target.value });
   }
 
   render() {
-    const { profile } = this.state;
+    const { charSearch } = this.state;
     return (
       <div>
-        <h1>{profile.displayName}</h1>
-        <button
-          type="button"
-          onClick={this.getProfile}
-        >
+        <label htmlFor="search">Profile Name:</label>
+        <input id="search" type="text" value={charSearch} onChange={this.handleInput} />
+        <button type="button" onClick={this.getProfile}>
           Get Profile
         </button>
         <div>
-          <h1>Placeholder</h1>
-          <button
-            type="button"
-            onClick={this.getManifest}
-          >
-            Manifest
+          <button type="button" onClick={this.getHistoricalStats}>
+            Historical Stats
           </button>
         </div>
         <div>
-          <button
-            type="button"
-            onClick={this.getHistoricalStats}
-          >
-          Historical Stats
-          </button>
-        </div>
-        <div>
-          <button
-            type="button"
-            onClick={this.getCharacterData}
-          >
-          Get Char Data
+          <button type="button" onClick={this.getCharacterData}>
+            Get Char Data
           </button>
         </div>
       </div>
